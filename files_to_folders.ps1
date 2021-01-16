31a4de177f18e92e4146cf5e763a037cb52b5223
bluenyellow <reply77@protonmail.ch>
2021-01-16 19:13:38 +0100

    Add files via upload

---
+++ files_to_folders.ps1
@@ -0,0 +1,11 @@
+﻿$names=(Get-ChildItem -File).BaseName
+foreach($name in $names){mkdir $name}
+
+
+$films=Get-ChildItem -File
+
+foreach($film in $films){
+
+Move-Item $film.Name $film.BaseName
+
+}
